9ce77bf132d38c21e0dc3d26e44d407f25b9341c
Gehad <gehad.mhmd.96@gmail.com>
2022-06-09 17:45:35 +0200

    bastion private key downloud

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,8 +3,5 @@ terraform {
     bucket = "backend-jenkines-gahoda"
     key    = "terraform"
     region = "us-east-1"
-
-     access_key="AKIA6CGBRS3AL7BGOWKE"
-     secret_key="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"
   }
 }
