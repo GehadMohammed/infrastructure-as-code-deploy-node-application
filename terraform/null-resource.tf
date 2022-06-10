34f3b9881c0f078fcc360d0c62590d1f5dd3deaa
Gehad <gehad.mhmd.96@gmail.com>
2022-06-10 06:42:25 +0200

    edit null resource

--- terraform/null-resource.tf
+++ terraform/null-resource.tf
@@ -3,11 +3,11 @@ resource "null_resource" "out" {
     always_run = "${timestamp()}"
   }
   provisioner "local-exec" {
-    command = "echo  ${self.private_ip} > private-ip.txt"
+    command = "echo  ${aws_instance.application-01.private_ip} > private-ip.txt"
   }
   
    provisioner "local-exec" {
-    command = "echo ${self.public_ip} > public-ip.txt"
+    command = "echo ${aws_instance.bastion-01.public_ip} > public-ip.txt"
   }
 
     provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
