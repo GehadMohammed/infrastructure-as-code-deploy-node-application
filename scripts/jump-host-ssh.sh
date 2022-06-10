062e8b6c698ac76bc46b2cf470043b284002740a
Gehad <gehad.mhmd.96@gmail.com>
2022-06-10 06:14:31 +0200

    public-ip.txt: No such file or directory - solve

--- scripts/jump-host-ssh.sh
+++ scripts/jump-host-ssh.sh
@@ -1,11 +1,11 @@
 cat <<EOF > $HOME/.ssh/config
 host bastion
-   HostName `cat public-ip.txt`
+   HostName `cat ~/workspace/infrastructure-configuration-deploy-node-app/terraform/public-ip.txt`
    User ubuntu
    identityFile $HOME/.ssh/BastionKey.pem
    StrictHostKeyChecking=no
 host private_instance
-   HostName  `cat private-ip.txt`
+   HostName  `cat ~/workspace/infrastructure-configuration-deploy-node-app/terraform/private-ip.txt`
    user  ubuntu
    ProxyCommand ssh bastion -W %h:%p
    identityFile $HOME/.ssh/BastionKey.pem
