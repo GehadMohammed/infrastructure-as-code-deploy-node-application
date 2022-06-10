32f8a6b0c735d26b8f9f157a791b0a84b90fa10e
Gehad <gehad.mhmd.96@gmail.com>
2022-06-10 13:21:13 +0200

    apply

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -33,8 +33,8 @@ resource "aws_db_instance" "rds-gehad-01" {
   db_name              = "mydb"
   username             = "root"
   password             = "12345678"
-  parameter_group_name = aws_db_parameter_group.default.id
-  db_subnet_group_name = aws_db_subnet_group.default.id
+  parameter_group_name = aws_db_parameter_group.default-01.id
+  db_subnet_group_name = aws_db_subnet_group.default-01.id
   vpc_security_group_ids = [ aws_security_group.rds-sg.id ]
   publicly_accessible  = false
   skip_final_snapshot  = true
